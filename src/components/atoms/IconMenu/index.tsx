d63bf3b3ee1ef55ca8951c9b9d97c022be3ece6c
Lucas Moraes <lucas.moraes88@yahoo.com>
2023-07-21 17:58:19 -0300

    fix: Adjusts

--- src/components/atoms/IconMenu/index.tsx
+++ src/components/atoms/IconMenu/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { THEME } from '../../../theme';
 
-export const IconMenu = () => <Icon name="menu" size={20} color={THEME.COLORS.TABLE} />;
+export const IconMenu = () => <Icon name="menu" size={30} color={THEME.COLORS.TABLE} />;
